c736c20bdb4a4c57d41190c97ef9f816143b5ad2
ErgerSusha <ergersusha2@gmail.com>
2019-07-15 18:58:22 +0300

    Added future structure

--- src/WordViewComponent.js
+++ src/WordViewComponent.js
@@ -1,10 +1,36 @@
 'use strict';
 
-export default class WordViewComponent{
-    constructor(){
-        
+export default class WordViewComponent {
+
+    // allCharacters
+    // index
+    // alphabet
+    // groups
+
+    constructor(/* index, language, allCharacters */) {
+        // set props, set alphabet using language
     }
-    test(){
+
+    /*
+
+    getCurGroup() {
+        return find group, that contains current character
+    }
+
+    openChar() {
+        if (ev.detail === allCharacters[index]) {
+            index++;
+            if (index >= allCharacters.length) {
+                return 1;
+            }
+            return getCurGroup();
+        }
+        return 0;
+    }
+    
+    */
+
+    test() {
         return 'ok';
     }
 }
