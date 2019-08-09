769d3dd0470b86c455e985cf26c9c1b90b2af87a
pavladan <fatalfenker@gmail.com>
2019-08-09 12:28:24 +0300

    Add CharComponent

--- src/WordViewComponent.js
+++ src/WordViewComponent.js
@@ -2,20 +2,11 @@
 
 import alphabet from '../assets/chars-accordance.json'
 
-export default class WordViewComponent {
-
-    constructor(allCharacters = [], index = 0, language = 'english') {
-        // set props, set alphabet using language
-        this.alphabet = alphabet;
-        if (typeof allCharacters === 'string') {
-            this.allCharacters = allCharacters.split('');
-        } else if (typeof allCharacters === 'object') {
-            this.allCharacters = allCharacters;
-        } else {
-            this.allCharacters = [];
-        }
-        this.index = index >= 0 ? index : 0;
-        this.language = this.alphabet[language] ? language : 'english';
+export class CharComponent {
+    constructor(symbol, state) {
+        this.symbol = symbol;
+        this.state = undefined;
+        this.element = undefined;
 
         this.uniqueClass = 'easy-lang-word__';
         this.charClass = this.uniqueClass + 'char'
@@ -24,16 +15,60 @@ export default class WordViewComponent {
         this.hiddenClass = this.uniqueClass + 'hidden';
         this.hiddenSymbol = '•';
 
+        this._create();
+        this.setStateOnChar(state);
+    }
+
+    _create() {
+        this.element = document.createElement('span');
+        this.element.classList.add(this.charClass);
+    }
+
+    setStateOnChar(state = this.state) {
+        if (this.state === state) return;
+        this.element.classList.remove(this.activeClass, this.openClass, this.hiddenClass);
+        this.state = state;
+        switch (state) {
+            case 'open':
+                this.element.classList.add(this.openClass);
+                this.element.textContent = this.symbol;
+                break;
+            case 'hidden':
+                this.element.classList.add(this.hiddenClass);
+                this.element.textContent = this.hiddenSymbol;
+                break;
+            case 'active':
+                this.element.classList.add(this.activeClass);
+                this.element.textContent = this.hiddenSymbol;
+                break;
+        }
+    }
+}
+
+export default class WordViewComponent {
+    constructor(allCharacters = [], index = 0, language = 'english') {
+        this.alphabet = alphabet;
+        if (typeof allCharacters === 'string') {
+            this.allCharacters = allCharacters.split('');
+        } else if (typeof allCharacters === 'object') {
+            this.allCharacters = allCharacters;
+        } else {
+            this.allCharacters = [];
+        }
+
+        this.index = index >= 0 ? index : 0;
+        this.language = this.alphabet[language] ? language : 'english';
+
+
+
         this.word = undefined;
         this._createAllCharacters();
         this._create();
     }
 
-
-
     getCurGroup() {
         let curLang = this.alphabet[this.language];
-        let curCharacter = this.allCharacters[this.index].char;
+        let curCharacter = this.allCharacters[this.index].symbol;
         if (!curLang || !curCharacter) {
             return false
         }
@@ -84,45 +119,25 @@ export default class WordViewComponent {
         return 0;
     }
 
-    _updateCharsState(){
-        this.allCharacters.forEach((c,i)=>{
+    _updateCharsState() {
+        this.allCharacters.forEach((char, i) => {
             if (i < this.index) {
-                this._setStateOnChar(c,'open');
+                char.setStateOnChar('open');
             } else if (i == this.index) {
-                this._setStateOnChar(c,'active')
+                char.setStateOnChar('active')
             } else if (i > this.index) {
-                this._setStateOnChar(c,'hidden')
+                char.setStateOnChar('hidden')
             }
         })
     }
 
-    _setStateOnChar(character, state) {
-        if (character.state === state) return;
-        character.element.classList.remove(this.activeClass, this.openClass, this.hiddenClass);
-        character.state = state;
-        switch (state) {
-            case 'open':
-                character.element.classList.add(this.openClass);
-                character.element.textContent = character.char;
-                break;
-            case 'hidden':
-                character.element.classList.add(this.hiddenClass);
-                character.element.textContent = this.hiddenSymbol;
-                break;
-            case 'active':
-                character.element.classList.add(this.activeClass);
-                character.element.textContent = this.hiddenSymbol;
-                break;
-        }
-    }
-
-    openWord(){
+    openWord() {
         this.index = this.allCharacters.length;
         this._updateCharsState();
         return 1;
     }
 
-    hiddenWord(){
+    hiddenWord() {
         this.index = 0;
         this._updateCharsState();
         return this.getCurGroup();
@@ -130,34 +145,22 @@ export default class WordViewComponent {
 
     _createAllCharacters() {
         this.allCharacters = this.allCharacters.map((character, i) => {
-            let element = document.createElement('span');
-            let state = undefined;
-            element.classList.add(this.charClass)
+            let char;
             if (i < this.index) {
-                element.textContent = character;
-                element.classList.add(this.openClass)
-                state = 'open';
+                char = new CharComponent(character, 'open')
             } else if (i == this.index) {
-                element.textContent = this.hiddenSymbol;
-                element.classList.add(this.activeClass)
-                state = 'active';
+                char = new CharComponent(character, 'active')
             } else if (i > this.index) {
-                element.textContent = this.hiddenSymbol;
-                element.classList.add(this.hiddenClass)
-                state = 'hidden';
+                char = new CharComponent(character, 'hidden')
             }
-            return {
-                element: element,
-                char: character,
-                state: state
-            }
+            return char;
         })
     }
 
     _create() {
         this.word = document.createElement('span');
-        this.allCharacters.forEach(character => {
-            this.word.appendChild(character.element)
+        this.allCharacters.forEach(char => {
+            this.word.appendChild(char.element)
         })
     }
 
